91288f0602db4847e22b29d0de5201dc500c2e11
mikeriosphilly <mikerioshbg@gmail.com>
2026-03-10 14:20:43 -0400

    Finalizing og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.abctransportation.com',
+  site: 'https://abc-transportation-nexvel.vercel.app',
   integrations: [react()],
 
   vite: {
