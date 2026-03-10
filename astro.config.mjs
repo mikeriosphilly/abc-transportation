05a47a1f5f274770980170a57f30c6a69489e771
mikeriosphilly <mikerioshbg@gmail.com>
2026-03-10 13:15:56 -0400

    Polishing canonical URL's, fixing feature title font

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.abctransportation.com',
   integrations: [react()],
 
   vite: {
